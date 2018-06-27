858db9b5e4e54e965f621c6d0f5c0a388a96a920
master <kujira5go5@gmail.com>
2018-06-27 17:27:37 +0900

    read and play Django

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-
+"""
 urlpatterns = [
     path("",views.index, name = "index"),
     path("<int:question_id>/",views.detail,name = "detail"),
@@ -11,4 +11,12 @@ urlpatterns = [
     url(r"^(P<question_id>[0-9]+)/$",views.detail,name = "detail"),
     url(r"^(?P<question_id>[0-9]+)/results/$",views.results,name = "results"),
     url(r"^(?P<question_id>[0-9]+)/vote/$",views.vote,name = "vote"),
+]
+"""
+app_name = "polls"
+urlpatterns = [
+    path("",views.index,name="index"),
+    path("<int:question_id>/",views.detail, name = "detail"),
+    path("<int:question_id>/results/",views.results, name = "results"),
+    path("<int:question_id>/vote/",views.vote, name = "vote"),
 ]
